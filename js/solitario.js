3e2525a896ac723c995a83d074e36a76ad03e4fa
Camila Micaela Urquizo Suca <camila.urquizo@Camilas-MacBook-Pro.local>
2023-07-02 22:49:53 +0200

    updates front

--- js/solitario.js
+++ js/solitario.js
@@ -2,12 +2,9 @@
 // Array de palos
 let palos = ["viu", "cua", "hex", "cir"];
 // Array de número de cartas
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-//let numeros = [9, 10, 11, 12];
-
-// paso (top y left) en pixeles de una carta a la siguiente en un mazo
-let paso = 5;
+let numeros = [9, 10, 11, 12];
 
 // Tapetes
 let tapeteInicial = document.getElementById("inicial");
@@ -26,14 +23,13 @@ let mazoReceptor3 = [];
 let mazoReceptor4 = [];
 
 let mazos = {
-  'inicial': mazoInicial,
-  'sobrantes_receptor': mazoSobrantes,
-  'receptor1': mazoReceptor1,
-  'receptor2': mazoReceptor2,
-  'receptor3': mazoReceptor3,
-  'receptor4': mazoReceptor4
+  inicial: mazoInicial,
+  sobrantes_receptor: mazoSobrantes,
+  receptor1: mazoReceptor1,
+  receptor2: mazoReceptor2,
+  receptor3: mazoReceptor3,
+  receptor4: mazoReceptor4,
 };
-console.log('MAZOS',mazos['inicial'])
 // Contadores de cartas
 let contInicial = document.getElementById("contador_inicial");
 let contSobrantes = document.getElementById("contador_sobrantes_receptor");
@@ -42,6 +38,17 @@ let contReceptor2 = document.getElementById("contador_receptor2");
 let contReceptor3 = document.getElementById("contador_receptor3");
 let contReceptor4 = document.getElementById("contador_receptor4");
 let contMovimientos = document.getElementById("contador_movimientos");
+
+// usamos un objeto para poder mapear los elementos de una manera mas facil
+let contadores = {
+  contador_inicial: contInicial,
+  contador_sobrantes_receptor: contSobrantes,
+  contador_receptor1: contReceptor1,
+  contador_receptor2: contReceptor2,
+  contador_receptor3: contReceptor3,
+  contador_receptor4: contReceptor4,
+  contador_movimientos: contMovimientos,
+};
 
 // Tiempo
 let contTiempo = document.getElementById("contador_tiempo"); // span cuenta tiempo
@@ -77,8 +84,6 @@ function comenzarJuego() {
   setEvents("receptor4");
   setEvents("receptor4");
   setEvents("sobrantes_receptor");
-  console.log('MAZOS',mazos['inicial'])
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
 
   // Puesta a cero de contadores de mazos
   setContador(contReceptor1, 0);
@@ -86,63 +91,12 @@ function comenzarJuego() {
   setContador(contReceptor3, 0);
   setContador(contReceptor4, 0);
   setContador(contSobrantes, 0);
-  console.log("mazoInicial", mazoInicial);
-  console.log("mazoSobrante", mazoSobrantes);
 
   // Arrancar el conteo de tiempo
-  startTimer();
+  arrancarTiempo();
 } // comenzarJuego
 
-/**
-	Se debe encargar de arrancar el temporizador: cada 1000 ms se
-	debe ejecutar una función que a partir de la cuenta autoincrementada
-	de los segundos (segundos totales) visualice el tiempo oportunamente con el 
-	format hh:mm:ss en el contador adecuado.
-
-	Para descomponer los segundos en horas, minutos y segundos pueden emplearse
-	las siguientes igualdades:
-
-	segundos = truncar (   segundos_totales % (60)                 )
-	minutos  = truncar ( ( segundos_totales % (60*60) )     / 60   )
-	horas    = truncar ( ( segundos_totales % (60*60*24)) ) / 3600 )
-
-	donde % denota la operación módulo (resto de la división entre los operadores)
-
-	Así, por ejemplo, si la cuenta de segundos totales es de 134 s, entonces será:
-	   00:02:14
-
-	Como existe la posibilidad de "resetear" el juego en cualquier momento, hay que 
-	evitar que exista más de un temporizador simultáneo, por lo que debería guardarse
-	el resultado de la llamada a setInterval en alguna variable para llamar oportunamente
-	a clearInterval en su caso.   
-*/
-
-// function arrancarTiempo(){
-// 	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-// 	if (temporizador) clearInterval(temporizador);
-//     let hms = function (){
-// 			let seg = Math.trunc( segundos % 60 );
-// 			let min = Math.trunc( (segundos % 3600) / 60 );
-// 			let hor = Math.trunc( (segundos % 86400) / 3600 );
-// 			let tiempo = ( (hor<10)? "0"+hor : ""+hor )
-// 						+ ":" + ( (min<10)? "0"+min : ""+min )
-// 						+ ":" + ( (seg<10)? "0"+seg : ""+seg );
-// 			setContador(contTiempo, tiempo);
-//             segundos++;
-// 		}
-// 	segundos = 0;
-//     hms(); // Primera visualización 00:00:00
-// 	temporizador = setInterval(hms, 1000);
-// 	let contador_tiempo = document.getElementById('contador_tiempo');
-
-// 	(function looper() {
-//         contador_tiempo.innerHTML = temporizador
-//         setTimeout(looper, 1000);
-//     })();
-
-// } // arrancarTiempo
-
-function startTimer() {
+function arrancarTiempo() {
   let seconds = 0;
   let minutes = 0;
   let hours = 0;
@@ -278,16 +232,16 @@ function cargarTapeteInicial(numeros) {
  	Esta función debe incrementar el número correspondiente al contenido textual
    	del elemento que actúa de contador
 */
-function incContador(contador, baraja) {
-  setContador(contador, baraja.length);
+function incContador(contador, valor) {
+  setContador(contador, valor);
   /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
 } // incContador
 
 /**
 	Idem que anterior, pero decrementando 
 */
-function decContador(contador) {
-  setContador(contador, baraja.length);
+function decContador(contador, valor) {
+  setContador(contador, valor);
   /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! ***/
 } // decContador
 
@@ -316,7 +270,7 @@ function setEvents(containers) {
 
 // Función para el evento dragstart
 function dragStart(event) {
-  console.log('dragStart')
+  console.log("dragStart");
   event.dataTransfer.setData("text/plain", event.target.id);
   event.target.classList.add("dragging");
 }
@@ -324,29 +278,28 @@ function dragStart(event) {
 // Función para el evento dragenter
 function dragEnter(event) {
   event.preventDefault();
-  console.log('dragEnter')
+  console.log("dragEnter");
   event.target.classList.add("drag-over");
 }
 // Función para el evento dragover
 function dragOver(event) {
-  console.log('dragOver')
+  console.log("dragOver");
   event.preventDefault();
 }
 
 // Función para el evento dragleave
 function dragLeave(event) {
-  console.log('dragLeave')
+  console.log("dragLeave");
   event.target.classList.remove("drag-over");
 }
 // Función para el evento dragend
 function dragEnd(event) {
-  console.log('event', event)
-  console.log('dragEnd')
+  console.log("dragEnd");
   event.target.classList.remove("dragging");
 }
 // Función para el evento dragleave
 function dragLeave(event) {
-  console.log('dragLeave')
+  console.log("dragLeave");
   event.target.classList.remove("drag-over");
 }
 
@@ -360,9 +313,9 @@ function drop(event) {
   card.style.top = 25 + "px";
 
   if (event.target.id.includes("receptor")) {
-    console.log(event.target.id)
+    console.log(event.target.id);
     mazos[event.target.id].push(cardId);
-    console.log(mazos[event.target.id])
+    console.log(mazos[event.target.id]);
 
     let lastCard = event.target.lastElementChild;
     // Obtener el z-index de la última carta
@@ -371,11 +324,17 @@ function drop(event) {
     // Aumentar el z-index de la carta arrastrada
     card.style.zIndex = (lastCardIndex + 1).toString();
     event.target.appendChild(card);
+    incContador(
+      contadores[`contador_${event.target.id}`],
+      mazos[event.target.id].length
+    );
+    eliminarCarta(cardId);
+    console.log(mazos.inicial);
   } else {
     //en caso el elmento target sea la carta dentro del div receptor
     let divContenedor = document.getElementById(event.target.id).parentElement;
     mazos[divContenedor.id].push(cardId);
-    console.log(mazos[divContenedor.id])
+    console.log(mazos[divContenedor.id]);
     let lastCard = divContenedor.lastElementChild;
     // Obtener el z-index de la última carta
     let lastCardIndex = lastCard ? parseInt(lastCard.style.zIndex, 10) : 0;
@@ -383,5 +342,51 @@ function drop(event) {
     // Aumentar el z-index de la carta arrastrada
     card.style.zIndex = (lastCardIndex + 1).toString();
     divContenedor.appendChild(card);
+    incContador(
+      contadores[`contador_${divContenedor.id}`],
+      mazos[divContenedor.id].length
+    );
+    eliminarCarta(cardId);
+    console.log(mazos.inicial);
   }
+}
+
+function eliminarCarta(cardId) {
+  // Buscar el índice de la carta a eliminar en el mazoinicial
+  let indice = mazoInicial.indexOf(cardId);
+
+  // Verificar si el evento se encuentra en el array
+  if (indice !== -1) {
+    // Eliminar el evento del array usando el método splice()
+    mazoInicial.splice(indice, 1);
+  }
+  setContador(contInicial, mazoInicial.length);
+
+  if (mazoInicial.length == 0 && mazoSobrantes.length > 0) {
+    mazoInicial = [...mazoSobrantes];
+    mazoSobrantes.length = 0;
+    mazoInicial.forEach(function (combinacion, index) {
+      let card = createCard(combinacion);
+      card.style.top = index * 5 + "px";
+      card.style.left = index * 5 + "px";
+      card.draggable = true;
+      card.setAttribute("data-palo", combinacion.split("-")[1]);
+      card.setAttribute("data-numero", combinacion.split("-")[0]);
+      card.addEventListener("dragstart", dragStart);
+      card.addEventListener("dragend", dragEnd);
+
+      tapeteInicial.appendChild(card);
+      setContador(contInicial, mazoInicial.length);
+      removerCartasSobrantes();
+      setContador(contSobrantes, mazoSobrantes.length);
+    });
+  }
+}
+
+function removerCartasSobrantes() {
+  let imagenes = tapeteSobrantes.querySelectorAll("img");
+
+  imagenes.forEach(function (imagen) {
+    imagen.remove();
+  });
 }
